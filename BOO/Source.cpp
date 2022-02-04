53953f9aa91dfb660052dd86ebbd2d36c176c31c
Hatem011 <h.zakaria34567@gmail.com>
2022-02-04 21:57:25 +0200

    problem2

---
+++ BOO/Source.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	double a, b, c, result;
+	cin >> a >> b >> c;
+	result = (2 * a + 3 * b) * 5 * c;
+	cout <<result<<setprecision(6)<<endl;
+}
