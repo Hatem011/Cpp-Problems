8751c948a62b33e186113958084cc9d0dbbe5f8f
Hatem011 <h.zakaria34567@gmail.com>
2022-02-23 22:39:03 +0200

    problem6

---
+++ twonumbers/twonumbers/Source.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	double num1, num2;
+	cin >> num1>>num2;
+	double res = num1 / num2;
+	cout << "floor " << num1 << " / " << num2 << " = " << floor(res) << endl;
+	cout << "ceil " << num1 << " / " << num2 << " = " << ceil(res) << endl;
+	cout << "round " << num1 << " / " << num2 << " = " << round(res) << endl;
+}
